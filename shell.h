583e5a7842e9439a3312d12ab7ad1a8171121eb7
romain <romainleoulzec@gmail.com>
2025-04-23 19:16:04 +0200

    prototype

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+int _strlen(const char *s);
+void execute_command(char *command);
+
+#endif
