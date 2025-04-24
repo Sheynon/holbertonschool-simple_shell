2cf857dd7a365843ef823ea1956e2a6501505cff
Sheynon <labadie.jonatahan@hotmail.fr>
2025-04-24 14:01:42 +0200

    fixed - critical error who modified directly the getenv path

--- path_handler.c
+++ path_handler.c
@@ -5,27 +5,38 @@
 
 char *find_path(char *command)
 {
-	char *path = getenv("PATH");
-	char *dir, *full_path;
+	char *path = _getenv("PATH");
+	char *path_copy, *dir, *full_path;
 	size_t len;
 
 	if (!path || !command)
 		return (NULL);
 
-	dir = strtok(path, ":");
+	path_copy = _strdup(path);
+	if (!path_copy)
+		return (NULL);
+
+	dir = strtok(path_copy, ":");
 	while (dir != NULL)
 	{
-		len = strlen(dir) + strlen(command) + 2;
+		len = _strlen(dir) + strlen(command) + 2;
 		full_path = malloc(len);
 		if (!full_path)
+		{
+			free(path_copy);
 			return (NULL);
+		}
 
 		snprintf(full_path, len, "%s/%s", dir, command);
 		if (access(full_path, X_OK) == 0)
+		{
+			free(path_copy);
 			return (full_path);
+		}
 
 		free(full_path);
 		dir = strtok(NULL, ":");
 	}
+	free(path_copy);
 	return (NULL);
 }
